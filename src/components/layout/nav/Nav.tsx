37cea8fb0cd94c1b618e24a19902cd2d50b71982
oluwafemi.ayesoro <oluwafemi.ayesoro@geocloud.co>
2022-02-10 22:26:26 +0100

    fix: side modification

--- src/components/layout/nav/Nav.tsx
+++ src/components/layout/nav/Nav.tsx
@@ -9,13 +9,13 @@ const Nav = () => {
   return (
       <nav className="nav">
         <NavLink to={routes.company.path} className="nav__link">
-        <div><IconTags /></div> <div className="route-name">{routes.company.label}</div>
+        <div><IconCompanies/></div> <div className="route-name">{routes.company.label}</div>
         </NavLink>
         <NavLink to={routes.tags.path} className="nav__link">
         <div><IconTags /></div> <div className="route-name">{routes.tags.label}</div>
         </NavLink>
         <NavLink to={routes.news.path} className="nav__link">
-        <div><IconTags /></div> <div className="route-name">{routes.news.label}</div>
+        <div><IconNews /></div> <div className="route-name">{routes.news.label}</div>
         </NavLink>
       </nav>
   );
